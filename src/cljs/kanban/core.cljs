2afb6f86301806d5bbc3832738d83c215b1b6c51
Vesa-Matti Salo <jujix@users.noreply.github.com>
2018-03-17 15:10:21 +0200

    Add css and cards. Functionality still missing

--- src/cljs/kanban/core.cljs
+++ src/cljs/kanban/core.cljs
@@ -1,12 +1,44 @@
 (ns kanban.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as r]))
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def app-state
+  (r/atom {:columns [{:title "Todos"
+                      :cards [{:title "Learn about Reagent"}
+                              {:title "Tell my friends about Lambda Island"}]}
+                     {:title "Awesomize"
+                      :editing true
+                      :cards [{:title "Meditate"}
+                              {:title "Work out"
+                               :editing true}]}]}))
 
-(defn greeting []
-  [:h1 (:text @app-state)])
+(defn Card [card]
+  (if (:editing card)
+    [:div.card.editing [:input {:type "text" :value (:title card)}]]
+    [:div.card (:title card)]))
 
-(defn render []
-  (reagent/render [greeting] (js/document.getElementById "app")))
+(defn NewCard []
+  [:div.new-card
+   "+ add new card"])
+
+(defn Column [{:keys [title cards editing]}]
+  [:div.column
+   (if editing
+     [:input {:type "text" :value title}]
+     [:h2 title])
+   (for [c cards]
+     [Card c])
+   [NewCard]])
+
+(defn NewColumn []
+  [:div.new-column
+   "+ add new column"])
+
+(defn Board [state]
+  [:div.board
+   (for [c (:columns @state)]
+     [Column c])
+   [NewColumn]])
+
+(r/render [Board app-state] (js/document.getElementById "app"))
